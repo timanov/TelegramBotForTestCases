611c123d0a8f3948465222d52cdfd1196c7cba6e
Mikhail <mihailtimanov@gmail.com>
2022-06-20 08:29:29 +0300

    Добавление класса с статичными переменными, которые принадлежат к классу static и финальные

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -4,6 +4,8 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import static Controller.ControllerForCase.*;
+
 public class Bot extends TelegramLongPollingBot {
     final private String BOT_TOKEN = "5532935508:AAHYxI5beyRIaCPektSyQHReqpok6l-GncY";
     final private String BOT_NAME = "MishkaCitatyBot";
@@ -52,17 +54,25 @@ public class Bot extends TelegramLongPollingBot {
             case "/start":
                 response = "Привет! :)\n\n" +
                         "Получи рандомную проверку, которую можно применить для написания тестовых сценариев!\n\n " +
-                        "1. Жми /ApiAssert для проверки API. \n " +
-                        "2. Жми /RegisterFormAssert для проверки формы регистрации \n ";
+                        "1. Жми " + APIASSERT + " для проверки API. \n " +
+                        "2. Жми " + REGISTERFORMASSERT + " для проверки формы регистрации. \n " +
+                        "3. Жми " + INFO + " чтобы получить ссылки на полезные ресурсы для тестировщиков. \n" +
+                        "4. Жми " + CAT + " для получения картинки случайного котика.";
                 break;
-            case "/ApiAssert":
+            case APIASSERT:
                 response = String.valueOf(storage.apiTestValue.getAssertApi());
                 break;
-            case "/RegisterFormAssert":
+            case REGISTERFORMASSERT:
                 response = String.valueOf(storage.registerFormValue.getAssertRegistration());
                 break;
+            case INFO:
+                response = "http://proglang.su/java/ курс по Java Core";
+                break;
+            case CAT:
+                response = "Котика пока неть :c";
+                break;
             default:
-                response = "Сообщение не распознано";
+                response = DEFAULT;
                 System.out.println("The user wrote an incorrect message : " + textMsg);
                 break;
         }
---
+++ src/main/java/Controller/ControllerForCase.java
@@ -0,0 +1,17 @@
+package Controller;
+
+public class ControllerForCase {
+
+    public static final String START = "/start";
+
+    public static final String APIASSERT = "/ApiAssert";
+
+    public static final String REGISTERFORMASSERT = "/RegisterFormAssert";
+
+    public static final String CAT = "/Cat";
+
+    public static final String INFO = "/Info";
+
+    public static final String DEFAULT = "Сообщение не распознано, нажми " +
+            START + " для получения начального окна.";
+}
